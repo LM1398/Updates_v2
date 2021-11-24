f2a011aacfe5cd925510ba6f2e09cffb8070b198
LM1398 <88918397+LM1398@users.noreply.github.com>
2021-11-24 20:22:46 +0900

    [update] updating visuals associated with comments function (#22)

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     path("accounts/login/", views.LoginView.as_view(), name="login"),
     path("", include("blog.urls")),
     path("accounts/logout/", views.LogoutView.as_view(next_page="/"), name="logout"),
