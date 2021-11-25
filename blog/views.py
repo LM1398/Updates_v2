21f9c10496e90bb1abadf48c3ec34078e185fb53
LM1398 <88918397+LM1398@users.noreply.github.com>
2021-11-25 22:54:13 +0900

    temporary commit - website still failing (#26)

--- blog/views.py
+++ blog/views.py
@@ -43,7 +43,7 @@ def post_edit(request, pk):
             return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, "blog/post_edit.html", {"form": form})
+    return render(request, "blog/post_edit.html", {"forms": form})
 
 
 @login_required
